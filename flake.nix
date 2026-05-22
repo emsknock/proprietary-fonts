946ad8e5412aa394c2f5e324be3504274e25d964
emsknock <25252659+emsknock@users.noreply.github.com>
2026-05-22 23:37:35 +0000

    Add Apple emoji

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
 	inputs.nixpkgs.url = "github:nixos/nixpkgs";
 	outputs = inputs: let
 		system = "x86_64-linux";
-		font = ["mono-lisa" "dank-mono"];
+		font = ["mono-lisa" "dank-mono" "apple-emoji"];
 		pkgs = import inputs.nixpkgs {inherit system;};
 		make = font-name: {
 			lib,
